4cd697108c314c06f2c1444464db186d512cd9a5
Finnmeowtons <soriano.christianjose.m@gmail.com>
2025-01-31 18:51:16 +0800

    websocket

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
-const app = express();
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const port = 3000;
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer)
 
-app.use(express.json()); 
+io.on('connection', (socket) => {
+  console.log('New client connected'); 
+
+  socket.on('message', (data) =>{
+    console.log('Message received from ESP32:', data);
+    io.emit('flutter', data);
+  });
+});
+
+io.on('message', (data) => {
+  console.log('Message received from ESP32:', data);
+});
 
 app.get('/api/data', (req, res) => {
   res.json({ message: 'Hello from the server!' });
@@ -10,6 +25,8 @@ app.get('/api/data', (req, res) => {
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hi' });
+  io.emit('server_message', 'Hello from the server!');
+
 });
 
 app.post('/api/data', (req, res) => {
@@ -18,6 +35,6 @@ app.post('/api/data', (req, res) => {
   res.status(200).json({ message: 'Data received successfully!' });
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+httpServer.listen(port, () => {
+  console.log(`Server listening on ws://localhost:${port}`);
 });
